680ba9d222e372dd1817abec4bb15d7802d22ab0
Prana Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-02-18 13:49:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Webcam.set({
 });
 Webcam.attach("#camera");
 
-ml5.imageClassifier(
+var classifier = ml5.imageClassifier(
   "https://teachablemachine.withgoogle.com/models/661fFI5qw/model.json",
   modelLoaded
 );
@@ -16,17 +16,50 @@ function takeSnapshot() {
     img = "<img id='image' src='" + data_uri + "' />";
     document.getElementById("snapshot").innerHTML = img;
   });
-  speak();
 }
 
 function modelLoaded() {
   console.log("Model Loaded!");
 }
 
-function speak() {
+function speak(pred1, pred2) {
   var synth = window.speechSynthesis;
-  text1 = "The first prediction is";
-  text2 = "The second prediction is";
+  text1 = "The first prediction is" + pred1;
+  text2 = "And The second prediction is" + pred2;
   var utterThis = new SpeechSynthesisUtterance(text1 + text2);
   synth.speak(utterThis);
 }
+
+function check() {
+  var myImg = document.getElementById("image");
+  classifier.classify(myImg, getData);
+}
+
+function getData(error, result) {
+  if(error) {
+    console.log(error);
+  }
+  else {
+    console.log(result);
+  var label = result[0].label;
+  var label2 = result[1].label;
+  document.getElementById("pred1").innerHTML = label;
+  document.getElementById("pred2").innerHTML = label2;
+
+  document.getElementById("emoji1").innerHTML = showEmoji(label);
+  document.getElementById("emoji2").innerHTML = showEmoji(label2);
+
+  speak(label, label2);
+  }
+}
+
+function showEmoji(label) {
+  if (label == "Happy") {
+    return "&#128512;";
+  } else if (label == "Sad") {
+    return "&#128532;";
+  } else if (label == "Angry") {
+    return "&#128546;";
+  }
+}
+
